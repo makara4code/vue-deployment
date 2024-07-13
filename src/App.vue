66692f19ef4ebd0cd2569e8fc1141bac5961587e
MAKARASAMA\Admin <nuolmakara@gmail.com>
2024-07-13 18:18:32 +0700

    revert to default config

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import VueImage from "@/assets/logo.svg"
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" :src="VueImage" width="500" height="500" />
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
